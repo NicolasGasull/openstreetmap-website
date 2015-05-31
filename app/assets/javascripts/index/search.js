6b0a8c4ee6c676a4abc7e93e358e55d44b85eb22
blint <nicolas@gasull.net>
2015-05-31 15:51:50 +0200

    Integrated hacked twitter's typeahead for Algolia seach suggestions

--- app/assets/javascripts/index/search.js
+++ app/assets/javascripts/index/search.js
@@ -1,14 +1,65 @@
 //= require jquery.simulate
+//= require typeahead.jquery.min.js
+//= require algoliasearch.jquery.min.js
 
 OSM.Search = function(map) {
-  $(".search_form input[name=query]")
-    .on("input", function(e) {
-      if ($(e.target).val() == "") {
-        $(".describe_location").fadeIn(100);
-      } else {
-        $(".describe_location").fadeOut(100);
+
+  // Algolia search suggestions
+  var algolia = {};
+
+  algolia.client = $.algolia.Client('977AC8JAJ4', '0958707ae59201f1fbf4c14b5397b7ab');
+  algolia.cities = algolia.client.initIndex('Cities');
+  algolia.search = $.throttle(100, searchSuggestions);
+
+  $(".search_form input[name=query]").on("input", function(e) {
+    if ($(e.target).val() == "") {
+      $(".describe_location").fadeIn(100);
+    } else {
+      $(".describe_location").fadeOut(100);
+    }
+  });
+
+  var $searchInput = $("#sidebar .search_form input[name=query]");
+  var $sidebar = $("#sidebar");
+
+  $searchInput.typeahead({
+      hint: false,
+      highlight: true
+    }, {
+      name: 'name',
+      source: algolia.cities.ttAdapter({hitsPerPage: 5}),
+      display: function(hit) {
+        return hit.name;
+      },
+      templates: {
+        suggestion: function(hit) {
+          return hit.name +
+            ' <span class="country">' + hit.countryCode + '</span>';
+        }
       }
-    })
+    });
+
+  // Move typeahead dropdown to the root of the sidebar, otherwise we'll have overflow issues
+  var sidebarOffset = $sidebar.offset();
+  var searchInputOffset = $searchInput.offset();
+  var $typeaheadWrapper = $searchInput.parent();
+
+  // Move search input back to its old wrapper
+  $typeaheadWrapper.parent().append($searchInput.detach());
+
+  // Absoltue position typeahead
+  $typeaheadWrapper = $typeaheadWrapper.detach();
+  $typeaheadWrapper.addClass("tt-search");
+  $typeaheadWrapper.width($searchInput.outerWidth());
+  $typeaheadWrapper.css("position", "absolute");
+  $typeaheadWrapper.css("z-index", 1500);
+  $typeaheadWrapper.offset({
+    top: searchInputOffset.top - sidebarOffset.top + $searchInput.outerHeight(),
+    left: searchInputOffset.left - sidebarOffset.left
+  });
+
+  $sidebar.append($typeaheadWrapper);
+
 
   $("#sidebar_content")
     .on("click", ".search_more a", clickSearchMore)
@@ -81,6 +132,17 @@ OSM.Search = function(map) {
 
     e.preventDefault();
     e.stopPropagation();
+  }
+
+  function searchSuggestions($target, cityArg) {
+
+    algolia.index.search(cityArg)
+      .then(function searchDone(content) {
+        console.log(content);
+      })
+      .fail(function (err) {
+        console.error(err);
+      });
   }
 
   var markers = L.layerGroup().addTo(map);
